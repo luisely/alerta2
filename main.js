60bdfccb825fc08b5b3c70112d6788fbb45cd76d
Luis Fernando Ely <luis.fernando@imdepa.com.br>
2019-05-21 08:11:06 -0300

    novos arquivos

---
+++ main.js
@@ -0,0 +1,93 @@
+const { app, Menu, Tray, BrowserWindow } = require('electron');
+const request = require('request');
+
+
+let tray = null;
+app.on('window-all-closed', e => e.preventDefault())
+
+
+
+/*
+var recursiva = function () {
+    request('http://192.168.1.56/arquivos/avisos/electron.txt', function (error, response, body) {
+      //console.error('error:', error); // Print the error if one occurred
+      //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      global.status = body // Print the HTML for the Google homepage.
+      console.log(global.status)
+    })
+    setTimeout(recursiva, 10000);
+  }
+  recursiva();
+  */
+app.on('ready', () => {
+
+    alertaWindow = new BrowserWindow({
+        alwaysOnTop: true,
+        width: 1100,
+        height: 600,
+        titleBarStyle: 'hidden',
+        maximizable: false,
+        fullscreenable: false,
+        show: false,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    })
+
+    alertaWindow.on('close', function (event) {
+        event.preventDefault();
+        alertaWindow.hide();
+    });
+
+    //alertaWindow.hide()
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Quit', click: () => { app.quit(); } }
+    ]);
+    tray = new Tray('ok.ico');
+    tray.setContextMenu(contextMenu);
+
+    const getStatus = () => {
+        //console.log('iniciou o update')
+        if (BrowserWindow.getAllWindows() >= 1) {
+            alertaWindow.hide()
+        }
+        request({
+            url: 'http://192.168.1.56/arquivos/avisos/electron.txt',
+            //headers: { 'CB-VERSION': '2017-08-04' },
+        }, (err, res, body) => {
+            if (err) {
+                tray.setToolTip('Error getting market price.');
+            } else {
+                const status = body;
+                tray.setToolTip(`Status: ${status}`);
+                if (status == 1) {
+                    tray.setImage('fail.ico')
+                    alertaWindow.setIcon('fail.ico')
+                    alertaWindow.setMenuBarVisibility(false)
+                    alertaWindow.loadURL('http://infra.imdepa.com.br/aviso.html')
+                    alertaWindow.show()
+                    //console.log('Aqui')
+                }
+                if (status == 2) {
+                    tray.setImage('ok.ico')
+                    alertaWindow.setIcon('ok.ico')
+                    alertaWindow.setMenuBarVisibility(false)
+                    alertaWindow.loadURL('http://infra.imdepa.com.br/liberado.html')
+                    alertaWindow.show()
+                }
+                if (status == 0) {
+                    let telasAbertas = BrowserWindow.getAllWindows()
+                    if (telasAbertas >= 1) {
+                        alertaWindow.hide()
+                    }
+                    tray.setImage('ok.ico')
+                    //console.log(telasAbertas)
+                }
+            }
+        });
+        setTimeout(getStatus, 200000);
+        //console.log('Acabou o update')
+    };
+    getStatus();
+});
